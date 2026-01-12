7b219179b3bd6881c4236116926ea6e3953d750e
Arjun Chavan <arjunchavhan8459@gmail.com>
2026-01-12 18:26:47 +0530

    PNG Logo Added

--- MyPortfolioFrontend/src/components/Nav.jsx
+++ MyPortfolioFrontend/src/components/Nav.jsx
@@ -30,7 +30,7 @@ const Navbar = () => {
           {/* LOGO */}
           <div className="flex items-center gap-3">
             <div className="w-10 h-10 rounded-full  flex items-center justify-center text-white font-bold">
-              <img src="/mynamelogo1.png" alt="My Portfolio Logo">
+              <img src="/mynamelogo1.png" alt="My Portfolio Logo"/>
             </div>
             <span className="text-xl font-semibold text-gray-900">
               Arjun Chavhan
